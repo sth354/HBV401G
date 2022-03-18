657572d744a6b4cece91faf84bb449e4f80d4992
frg13 <92691773+frg13@users.noreply.github.com>
2022-03-18 15:34:06 +0000

    Freyr first commit

--- DayTourSearchEngine/src/main/java/Controllers/HelloApplication.java
+++ DayTourSearchEngine/src/main/java/Controllers/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
+    //Comment test Freyr
     public static void main(String[] args) {
         launch();
     }
