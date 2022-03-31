fededaedd6f6ecedb1588baf0a777849b1e01f42
frg13 <92691773+frg13@users.noreply.github.com>
2022-03-31 10:31:17 +0000

    Model klasar med tilviksbreytum, getters og setters

--- DayTourSearchEngine/src/main/java/Model/User.java
+++ DayTourSearchEngine/src/main/java/Model/User.java
@@ -1,4 +1,47 @@
 package Model;
 
 public class User {
+    private String name;
+    private String emailAddress;
+    private String password;
+    private boolean moderator;
+
+    public User(String name, String emailAddress, String password, boolean moderator) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.password = password;
+        this.moderator = moderator;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isModerator() {
+        return moderator;
+    }
+
+    public void setModerator(boolean moderator) {
+        this.moderator = moderator;
+    }
 }
