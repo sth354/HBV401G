7ad2f5343ff39ced0934f537363b7ecc867a5245
sth354 <72753260+sth354@users.noreply.github.com>
2022-03-31 09:16:34 +0000

    Update ToursController.java

--- DayTourSearchEngine/src/main/java/Controllers/ToursController.java
+++ DayTourSearchEngine/src/main/java/Controllers/ToursController.java
@@ -6,8 +6,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.util.Locale;
-
 public class ToursController {
     private TourDB tours;
 
